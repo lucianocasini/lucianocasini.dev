370950caac89d55f94d1908231f75c454d060f13
lucianocasini <lucianoo.casini@gmail.com>
2023-01-14 04:19:53 -0300

    Add welcome section responsive styles

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,12 +3,12 @@ import '../assets/styles/Welcome.scss';
 
 const Welcome = () => {
   return (
-    <div className="Welcome">
+    <section id="welcome">
       <div className="name">
         <h1>Luciano Casini</h1>
         <h2>FullStack Developer</h2>
       </div>
-    </div>
+    </section>
   );
 };
 
