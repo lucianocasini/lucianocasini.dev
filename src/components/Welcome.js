c539290839575a3ef38de64b540b33d04f0276aa
lucianocasini <lucianoo.casini@gmail.com>
2023-01-14 05:26:12 -0300

    Add scroll message on responsive

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../assets/styles/Welcome.scss';
+import { BsChevronDoubleDown } from 'react-icons/bs';
 
 const Welcome = () => {
   return (
@@ -7,6 +8,10 @@ const Welcome = () => {
       <div className="name">
         <h1>Luciano Casini</h1>
         <h2>FullStack Developer</h2>
+      </div>
+      <div className="scroll">
+        <span>Desliza para leer un poco sobre mí</span>
+        <BsChevronDoubleDown />
       </div>
     </section>
   );
