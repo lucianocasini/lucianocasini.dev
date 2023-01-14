98df2c3daab82efa6ddd5ee833c3520b009bcc59
lucianocasini <lucianoo.casini@gmail.com>
2023-01-14 03:00:12 -0300

    Add scroll to anchor function and load projects images from local

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './assets/styles/general.css';
 import AboutMe from './components/AboutMe';
 import { Contact } from './components/Contact';
@@ -9,13 +9,28 @@ import Projects from './components/Projects';
 import Welcome from './components/Welcome';
 
 function App() {
+  const [imagesLoaded, setImagesLoaded] = useState(0);
+
+  const onLoadImage = () => {
+    setImagesLoaded((prevImages) => prevImages + 1);
+  };
+
+  useEffect(() => {
+    const currentLocation = window.location.href;
+    if (imagesLoaded === 3 && currentLocation.includes('/#')) {
+      const anchor = currentLocation.substring(
+        currentLocation.indexOf('#') + 1
+      );
+      document.getElementById(anchor).scrollIntoView();
+    }
+  }, [imagesLoaded]);
   return (
     <div id="main">
       <Welcome />
       <Navbar />
       <AboutMe />
       <Experience />
-      <Projects />
+      <Projects onLoadImage={onLoadImage} />
       <Contact />
       <Footer />
     </div>
